b3928f9101e75a67112731ad276742f522c44497
biondi27 <122488988+biondi27@users.noreply.github.com>
2023-03-08 21:17:52 -0400

    Add files via upload

---
+++ Polimorfismo/Polimorfismo/Classes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public abstract class Vehiculo
+    {
+        public virtual string Arrancar()
+        {
+            return "Su vehículo está arrancando.";
+        }
+        public abstract string Apagar();
+        public abstract string Acelerar();
+        public abstract string Cruise();
+        public abstract string Desacelerar();
+        public abstract string Estacionar();
+    }
+    public class Ferrari : Vehiculo
+    {
+        public override string Arrancar()
+        {
+            return "El Ferrari está encendido.";
+        }
+        public override string Apagar()
+        {
+            return "El Ferrari está apagado.";
+        }
+        public override string Acelerar()
+        {
+            return "El Ferrari está acelerando.";
+        }
+        public override string Cruise()
+        {
+            return "El Ferrari está viajando a una velocidad constante.";
+        }
+        public override string Desacelerar()
+        {
+            return "El Ferrari está desacelerando.";
+        }
+        public override string Estacionar()
+        {
+            return "El Ferrari está estacionando.";
+        }
+    }
+    public class Porsche : Vehiculo
+    {
+        public override string Arrancar()
+        {
+            return "Encendiendo el Porsche...";
+        }
+        public override string Apagar()
+        {
+            return "Ha terminado el viaje y se ha apagado el Porsche.";
+        }
+        public override string Acelerar()
+        {
+            return "Comenzando a manejar el Porsche y acelerando.";
+        }
+        public override string Cruise()
+        {
+            return "Manejando el Porsche a una velocidad rápida y veloz.";
+        }
+        public override string Desacelerar()
+        {
+            return "Desacelerando el Porsche y buscando parqueo.";
+        }
+        public override string Estacionar()
+        {
+            return "Estacionando el Porsche...";
+        }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Polimorfismo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ferrari ferrari1 = new Ferrari();
+            Porsche porsche1 = new Porsche();
+
+            Vehiculo[] garaje = { ferrari1, porsche1 };
+
+            Console.WriteLine("Entrando al Garaje para realizar viajes de prueba con los nuevos vehículos.");
+            Console.WriteLine("Presione cualquier tecla para iniciar los viajes de prueba.");
+            Console.WriteLine("Presione al finalizar el primer viaje para iniciar el segundo.");
+            Console.ReadKey();
+
+            foreach (Vehiculo item in garaje)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine(item.Arrancar());
+                Console.WriteLine(item.Acelerar());
+                Console.WriteLine(item.Cruise());
+                Console.WriteLine(item.Desacelerar());
+                Console.WriteLine(item.Estacionar());
+                Console.WriteLine(item.Apagar());
+                Console.WriteLine("---------------------------");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Así concluyen los viajes de prueba.");
+            Console.WriteLine("Los vehículos nuevos funcionan bien y todo parece en órden.");
+            Console.WriteLine("Presione cualquier tecla para finalizar la sesión.");
+            Console.ReadKey();
+        }
+    }
+}
